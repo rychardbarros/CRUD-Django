78fbe12e4690c8778a5a45e50db7b7a9aa0f2392
rychardbarros <rychard.barros.dev@gmail.com>
2023-07-27 16:36:06 -0300

    'save'

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, save
 
 urlpatterns = [
-    path('/teste',home)
+    path('', home), 
+    path('salvar/', save, name='save'), 
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from .models import Person
 def home(request):
     person = Person.objects.all()
     return render(request, 'index.html', {'person': person})
+
+def save(request):
+    name = request.POST.get('person_name')
+    Person.objects.create(name=name)
+    persons = Person.objects.all()
+    return render(request, 'index.html', {'person': persons})
