d3640f9231900a82d3123e189f1bdd97ad9ddf53
rychardbarros <rychard.barros.dev@gmail.com>
2023-07-28 13:51:07 -0300

    'crud_concluido'

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home, save
+from .views import home, save, update, edit, delete
 
 urlpatterns = [
     path('', home), 
-    path('salvar/', save, name='save'), 
+    path('salvar/', save, name='save'),
+    path('editar/<int:id>', edit, name='edit'),
+    path('update/<int:id>', update, name='update'),
+    path('deletar/<int:id>', delete, name='delete'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
 
 def home(request):
@@ -10,3 +10,19 @@ def save(request):
     Person.objects.create(name=name)
     persons = Person.objects.all()
     return render(request, 'index.html', {'person': persons})
+
+def edit(request, id):
+    person = Person.objects.get(id=id)
+    return render(request, 'update.html', {'person': person})
+
+def update(request, id):
+    name = request.POST.get('person_name')
+    person = Person.objects.get(id=id)
+    person.name = name
+    person.save()
+    return redirect(home)
+
+def delete(request, id):
+    person = Person.objects.get(id=id)
+    person.delete()
+    return redirect(home)
