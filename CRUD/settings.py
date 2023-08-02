a84515a6eae63372948cf7d9b5c58b745a4e5f34
RycharDev <rychard.barros.dev@gmail.com>
2023-08-01 22:34:48 -0300

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qi=o$_*(^w7ez)7m(4_f-8^i$=(cljh4dqg39gyq^q-l#gbkt1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
